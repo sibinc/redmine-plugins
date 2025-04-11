60d217bc79606ad2591e4789ff2a9c91feb348d4
sibinc <sibin@linways.com>
2025-04-11 12:48:44 +0530

    fix: added redmine linways plugin; ;

--- custom_api/config/routes.rb
+++
@@ -1,6 +0,0 @@
-# Plugin's routes
-# See: http://guides.rubyonrails.org/routing.html
-Rails.application.routes.draw do
-    get 'custom_api/allowed_statuses/:id', to: 'custom_api#allowed_statuses', defaults: { format: 'json' }
-  end
-  
